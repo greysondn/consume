d751275295d4faa4d4bc9b0032c4f15a16d46aee
J. 'Dorian Greyson' L <greysondn@gmail.com>
2019-01-20 06:03:00 -0500

    TitleState - background now matches classic game

--- source/net/darkglass/consume/TitleState.hx
+++ source/net/darkglass/consume/TitleState.hx
@@ -1,12 +1,22 @@
 package net.darkglass.consume;
 
-import flixel.FlxState;
+import flixel.addons.ui.FlxUIState;
+import flixel.addons.ui.FlxUISprite;
 
-class TitleState extends FlxState
+class TitleState extends FlxUIState
 {
     override public function create():Void
     {
+        // get this out of the way
         super.create();
+
+        // now ui
+        
+        // background
+        var titlebg:FlxUISprite = new FlxUISprite(0, 0);
+        //                              AARRGGBB   For alpha, FF is opaque and 00 is transparent
+        titlebg.makeGraphic(850, 640, 0xFFFFFFFF);
+        this.add(titlebg);
     }
 
     override public function update(elapsed:Float):Void
