0a316896e2cc60a2619785471b13c49d58373249
J. 'Dorian Greyson' L <greysondn@gmail.com>
2019-01-22 01:27:37 -0500

    OptionsSubstate.hx - pretty sure that's enough to start into it anyway

---
+++ source/net/darkglass/consume/substate/OptionSubstate.hx
@@ -0,0 +1,11 @@
+package net.darkglass.consume.substate;
+
+import flixel.addons.ui.FlxUISubState;
+
+class OptionSubstate extends FlxUISubState
+{
+    override public function create():Void 
+    {
+        super.create();
+    }
+}
