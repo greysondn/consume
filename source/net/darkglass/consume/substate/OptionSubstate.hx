1f27a0edad1e1a7796cb06dc72c9d23e69962305
J. 'Dorian Greyson' L <greysondn@gmail.com>
2019-01-22 02:06:03 -0500

    OptionSubstate - list of what is needed

--- source/net/darkglass/consume/substate/OptionSubstate.hx
+++ source/net/darkglass/consume/substate/OptionSubstate.hx
@@ -6,6 +6,28 @@ class OptionSubstate extends FlxUISubState
 {
     override public function create():Void 
     {
+        // buttons needed:
+        //
+        // title screen
+        // ------------
+        // Scat
+        // Arousal
+        // Debug
+        // Font Size+
+        // Font Size-
+        // Difficulty +
+        // Difficulty -
+        // NPC Gender
+        //
+        // ingame
+        // -----------------
+        // back to Main Menu
+        // Save
         super.create();
+
+        // why don't we organize these options? ... But
+        // first! ... We'll need to build a window proper
+        // for them. So that's my first task, I suppose.
+
     }
 }
