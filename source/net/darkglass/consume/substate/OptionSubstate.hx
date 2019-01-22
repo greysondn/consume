4a2109bd953f50512da7678880236983347c648a
J. 'Dorian Greyson' L <greysondn@gmail.com>
2019-01-22 03:50:47 -0500

    OptionSubstate - add back button

--- source/net/darkglass/consume/substate/OptionSubstate.hx
+++ source/net/darkglass/consume/substate/OptionSubstate.hx
@@ -5,6 +5,7 @@ import flixel.addons.ui.FlxUISubState;
 import flash.geom.Rectangle;
 
 import flixel.addons.ui.FlxUI9SliceSprite;
+import flixel.addons.ui.FlxUIButton;
 
 class OptionSubstate extends FlxUISubState
 {
@@ -37,7 +38,21 @@ class OptionSubstate extends FlxUISubState
         var buttonClickImg:String     = "assets/images/gui/classic/nineslice/window-click.png";
         var buttonDisabledImg:String  = "assets/images/gui/classic/nineslice/window-disabled.png";
 
+        var buttonEnabledGFX:Array<String>  = [buttonNormalImg, buttonHoverImg, buttonClickImg];
+        var buttonDisabledGFX:Array<String> = [buttonDisabledImg, buttonDisabledImg, buttonDisabledImg];
+        var slicecoords:Array<Array<Int>> = [[1, 1, 2, 2], [1, 1, 2, 2], [1, 1, 2, 2]];
+
         var background:FlxUI9SliceSprite = new FlxUI9SliceSprite(23, 23, buttonNormalImg, new Rectangle(0, 0, 804, 594), [1, 1, 2, 2]);
         this.add(background);
+
+        // clickable button that doesn't suck too hard to close it?
+        var backButton:FlxUIButton = new FlxUIButton(32, 566, "Back", onClick_back);
+        backButton.loadGraphicSlice9(buttonEnabledGFX, 786, 42, slicecoords, false, -1);
+        this.add(backButton);
+    }
+
+    public function onClick_back():Void
+    {
+        this.close();
     }
 }
