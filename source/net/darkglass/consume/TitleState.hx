a1a363215d12a7574015275cf8fdac09eaca86f4
J. 'Dorian Greyson' L <greysondn@gmail.com>
2019-02-07 16:37:01 -0500

    TitleState - it's now v0.53pre1!

--- source/net/darkglass/consume/TitleState.hx
+++ source/net/darkglass/consume/TitleState.hx
@@ -50,6 +50,10 @@ class TitleState extends FlxUIState
         // version
         // pos 489x42
         // sz 162x97
+        var verText:FlxUIText = new FlxUIText(551, 227, 162, "v0.53pre1\n7 Feb 2019");
+        verText.alignment = "center";
+        verText.addFormat(fntcol);
+        this.add(verText);
         
         // creators
         // pos 190x293
