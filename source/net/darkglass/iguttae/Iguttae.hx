086a358a714210e76ba1eec5e632e2948bc70d76
J. 'Dorian Greyson' L <greysondn@gmail.com>
2019-02-14 05:21:50 -0500

    Iguttae.hx - stub so we can write code around this.

---
+++ source/net/darkglass/iguttae/Iguttae.hx
@@ -0,0 +1,19 @@
+package net.darkglass.iguttae;
+
+/**
+ * IGUTTAE - I give up, this is a text adventure engine
+ * 
+ * The engine designed for the rework of Consume.
+ */
+class Iguttae 
+{
+    public function new()
+    {
+        // do nothing
+    }
+
+    public function eval(input:String):Void
+    {
+        // pass
+    }
+}
