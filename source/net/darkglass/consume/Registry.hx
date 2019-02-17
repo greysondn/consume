ddb7ee42d2f1ce0683db65a6c0fd6f449ab12214
J. 'Dorian Greyson' L <greysondn@gmail.com>
2019-02-17 05:48:02 -0500

    Registry.hx - replace tabs with spaces

--- source/net/darkglass/consume/Registry.hx
+++ source/net/darkglass/consume/Registry.hx
@@ -79,72 +79,72 @@ class Registry
      */
     // public var gameSaveName:String = "Consume";
 
-	//The following values are saved
-	// public var debugMode:Bool = false;
+    //The following values are saved
+    // public var debugMode:Bool = false;
 
-	/**
-	 * Whether or not user has arousal enabled.
-	 */
-	public var arousalEnabled:Bool = true;
+    /**
+     * Whether or not user has arousal enabled.
+     */
+    public var arousalEnabled:Bool = true;
 
-	/**
-	 * Whether or not user has scat enabled
-	 */
-	public var scatEnabled:Bool = true;
+    /**
+     * Whether or not user has scat enabled
+     */
+    public var scatEnabled:Bool = true;
 
-	/**
-	 * Deprecated - Whether or not sex is allowed to happen. Use arousalEnabled instead!
-	 */
-	public var allowSex(get, set):Bool;
+    /**
+     * Deprecated - Whether or not sex is allowed to happen. Use arousalEnabled instead!
+     */
+    public var allowSex(get, set):Bool;
 
-	 /**
-	  * Deprecated - Whether or not scat is allowed to happen. Use scatEnabled instead!
-	  */
-	 public var allowScat(get, set):Bool;
+     /**
+      * Deprecated - Whether or not scat is allowed to happen. Use scatEnabled instead!
+      */
+     public var allowScat(get, set):Bool;
 
-	// public var textSize:Int = 17;
-	// public var currentRoomID:Int;
+    // public var textSize:Int = 17;
+    // public var currentRoomID:Int;
 
-	/**
-	 * Combat Difficulty
-	 */
-	public var difficulty:Float = 1;
+    /**
+     * Combat Difficulty
+     */
+    public var difficulty:Float = 1;
 
-	//This value tells the game if the save data is going to be missing values so we don't look for them. Saves with a buldNumer lower then 11 are not compatable with this game engine at all.
-	// public var buildNumber:Int = 20;
-	// public var minBuildNumber:Int = 20; //If the saved build number is below this number, don't allow players to load that file. Otherwise the game will attempt to update the file
-	
-	// public var perkCostMultiplier:Int = 13;
-	
-	//Changes the number that displays in the corner of the play field
-	// public var buildVersion:String = "0.53.pre";
-	
-	//Global vars holding temp data
-	// public var backTo:String;
-	// public var conversationStep:Int;
-	// public var sellItem:Int;
-	// public var secretPress:UInt;
-	// public var lastRoom:Int = -1;
-	// public var prevKey:String = "";
-	
-	// public var shopLists:Array<Dynamic>;
-	
-	// public var gymFee:Int = 20; //Cost to use the gym, futureproofing
-	
-	//Player min and max values
-	// public var maxHeight:Int = 120; //Max player height, in inches: 10'
-	// public var minHeight:Int = 24; //Min player height, in inches: 2'
-	// public var minChest:Int = 12;
-	// public var maxChest:Int = 60;
-	// public var minWaist:Int = 8;
-	// public var maxWaist:Int = 60;
-	// public var minHips:Int = 10;
-	// public var maxHips:Int = 70;
-	
-	//Blackjack
-	// public var betAmount:Float = 0; //Blackjack bet amount
-	// public var betTarget:String = ""; //Blackjack bet target
-	
+    //This value tells the game if the save data is going to be missing values so we don't look for them. Saves with a buldNumer lower then 11 are not compatable with this game engine at all.
+    // public var buildNumber:Int = 20;
+    // public var minBuildNumber:Int = 20; //If the saved build number is below this number, don't allow players to load that file. Otherwise the game will attempt to update the file
+    
+    // public var perkCostMultiplier:Int = 13;
+    
+    //Changes the number that displays in the corner of the play field
+    // public var buildVersion:String = "0.53.pre";
+    
+    //Global vars holding temp data
+    // public var backTo:String;
+    // public var conversationStep:Int;
+    // public var sellItem:Int;
+    // public var secretPress:UInt;
+    // public var lastRoom:Int = -1;
+    // public var prevKey:String = "";
+    
+    // public var shopLists:Array<Dynamic>;
+    
+    // public var gymFee:Int = 20; //Cost to use the gym, futureproofing
+    
+    //Player min and max values
+    // public var maxHeight:Int = 120; //Max player height, in inches: 10'
+    // public var minHeight:Int = 24; //Min player height, in inches: 2'
+    // public var minChest:Int = 12;
+    // public var maxChest:Int = 60;
+    // public var minWaist:Int = 8;
+    // public var maxWaist:Int = 60;
+    // public var minHips:Int = 10;
+    // public var maxHips:Int = 70;
+    
+    //Blackjack
+    // public var betAmount:Float = 0; //Blackjack bet amount
+    // public var betTarget:String = ""; //Blackjack bet target
+    
     // -------------------------------------------------------------------------
     // Random NPC properties
     // -------------------------------------------------------------------------
@@ -291,17 +291,17 @@ class Registry
     // Not NPC stuff
     // -------------------------------------------------------------------------
 
-	function set_allowSex(newState:Bool)
-	{
-		FlxG.log.warn("Call made to set_allowSex!");
-		return this.arousalEnabled = newState;
-	}
+    function set_allowSex(newState:Bool)
+    {
+        FlxG.log.warn("Call made to set_allowSex!");
+        return this.arousalEnabled = newState;
+    }
 
-	function get_allowSex()
-	{
-		FlxG.log.warn("Call made to get_allowSex!");
-		return this.arousalEnabled;
-	}
+    function get_allowSex()
+    {
+        FlxG.log.warn("Call made to get_allowSex!");
+        return this.arousalEnabled;
+    }
 
     function set_allowScat(newState:Bool)
     {
