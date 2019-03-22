fdc7ab74deac7fcd2ae693f1cf95b0dade0afbe8
J. 'Dorian Greyson' L <greysondn@gmail.com>
2019-03-22 00:13:52 -0400

    YamlMapLoader - start on a loader for Yaml format

---
+++ source/net/darkglass/iguttae/gameworld/map/YamlMapLoader.hx
@@ -0,0 +1,30 @@
+package net.darkglass.iguttae.gameworld.map;
+
+class YamlMapLoader 
+{
+    /**
+     * Singleton instance of class
+     */
+    private static var instance:YamlMapLoader;
+
+    /**
+     * Constructor.
+     */
+    private function new()
+    {
+        // dummy so we can have a singleton
+    }
+
+    /**
+     * Returns a working, good-as-new YamlMapLoader.
+     */
+    public static function create():YamlMapLoader
+    {
+        if (null == YamlMapLoader.instance)
+        {
+            YamlMapLoader.instance = new YamlMapLoader();
+        }
+        
+        return YamlMapLoader.instance;
+    }
+}
