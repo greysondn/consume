6a9a1befa79d3ff60455cc3fdc245d50192a3913
J. 'Dorian Greyson' L <greysondn@gmail.com>
2019-04-18 10:48:03 -0400

    Now with the magic of don't care, just gimme one

--- source/net/darkglass/iguttae/expression/GetExpression.hx
+++ source/net/darkglass/iguttae/expression/GetExpression.hx
@@ -32,21 +32,51 @@ class GetExpression extends BaseExpression
         var loc:Actor = actor.location;
 
         // what?
-        var item:String = this.removeFirstWord(input);
+        var what:String = this.removeFirstWord(input);
 
         // try to get it
-        var locItems:Array<Entity> = loc.inventory.getAllAnswering(item);
+        var locItems:Array<Entity> = loc.inventory.getAllAnswering(what);
+
+        // make item pool unique-ish
+        var locItemsUniq:Array<Entity> = [];
+
+        for (item in locItems)
+        {
+            // see if it's really unique 
+            var unique:Bool = true;
+
+            // yep...
+            for (uniq in locItemsUniq)
+            {
+                // so the two properties I'mma check are
+                // index and name. Figure if those match the dev
+                // is trying to break things.
+                if (item.name == uniq.name)
+                {
+                    if (item.index == uniq.index)
+                    {
+                        unique = false;
+                    }
+                }
+            }
+
+            // apparently it's unique
+            if (unique)
+            {
+                locItemsUniq.push(item);
+            }
+        }
 
         // check it
-        if (0 == locItems.length)
+        if (0 == locItemsUniq.length)
         {
             // nobody here but us chicken
-            env.outStream("There's no " + item + " here.");
+            env.outStream("There's no " + what + " here.");
         }
-        else if (1 == locItems.length)
+        else if (1 == locItemsUniq.length)
         {
             // only one, we can do this
-            var actualItem:Actor = cast(locItems[0], Actor);
+            var actualItem:Actor = cast(locItemsUniq[0], Actor);
 
             if (actor.inventory.add(actualItem))
             {
@@ -58,7 +88,7 @@ class GetExpression extends BaseExpression
             else
             {
                 // you couldn't do it
-                env.outStream("For whatever reason, you couldn't pick up " + item);
+                env.outStream("For whatever reason, you couldn't pick up " + what);
             }
         }
         else
@@ -70,7 +100,7 @@ class GetExpression extends BaseExpression
             // oh no!
             var itemList:String = "";
 
-            for (item in locItems)
+            for (item in locItemsUniq)
             {
                 itemList = itemList + "- " + item.name + "\n";
             }
