a21a4b7f448c5f1c569dfe960a2a721e3b629ced
J. 'Dorian Greyson' L <greysondn@gmail.com>
2019-05-14 23:54:32 -0400

    Room - move only remaining comment in

--- source/net/darkglass/iguttae/gameworld/map/Room.hx
+++ source/net/darkglass/iguttae/gameworld/map/Room.hx
@@ -3,6 +3,17 @@ package net.darkglass.iguttae.gameworld.map;
 import net.darkglass.iguttae.gameworld.actor.Actor;
 import net.darkglass.iguttae.environment.Environment;
 
+// note from old code
+//  /* Special button flags, each button can have one of the following flags on it
+//   * 0 - Nothing
+//   * 1 - Hunt, passive/active (Allows the player to hunt for prey. passive/active = club/park probably)
+//   * 2 - Shop, [shop invintory]
+//   * 3 - Talk, NPC (Chat with an NPC, should be an ID from roomNPCs. Eating NPCs will be handled here now)
+//   * 4 - Work, Time (Player works for time)
+//   * 5 - Toilet
+//   * 6 - Sleep
+//   */
+
 class Room extends Actor
 {
     /**
