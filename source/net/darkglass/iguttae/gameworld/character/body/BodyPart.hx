0acd1e627e6bf4dc418e47993187f29efc585a15
J. 'Dorian Greyson' L <greysondn@gmail.com>
2019-07-02 10:52:29 -0400

    BodyPart - now clonable

--- source/net/darkglass/iguttae/gameworld/character/body/BodyPart.hx
+++ source/net/darkglass/iguttae/gameworld/character/body/BodyPart.hx
@@ -35,4 +35,21 @@ class BodyPart
         this.name = _name;
         this.id   = _id;
     }
+
+    public function clone():BodyPart
+    {
+        // just spawn literally any old body part
+        var ret:BodyPart = new BodyPart("", "");
+
+        // copy properties into it
+        ret.id = this.id;
+        ret.isPhallic = this.isPhallic;
+        ret.name = this.name;
+        ret.pLength = this.pLength;
+        ret.pMultiplier = this.pMultiplier;
+        ret.pWidth = this.pWidth;
+
+        // return
+        return ret;
+    }
 }
