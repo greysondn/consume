a0a8ed5e957e7b3bbc5d860a3d7e4818680624f5
J. 'Dorian Greyson' L <greysondn@gmail.com>
2019-07-02 09:25:52 -0400

    BodyPart - slightly better way to do this I think

---
+++ source/net/darkglass/iguttae/gameworld/character/body/BodyPart.hx
@@ -0,0 +1,26 @@
+package net.darkglass.iguttae.gameworld.character.body;
+
+class BodyPart
+{
+    /**
+     * Name for this specific body part
+     */
+    public var name:String = "BP_DESC";
+
+    /**
+     * reference id for this body part when looking up
+     */
+    public var id:String = "BP_ID";
+
+    /**
+     * Creates a brand new spanking shiny body part
+     * 
+     * @param _id   the term to use to refer to this part for lookup
+     * @param _name the descriptor used for this body part
+     */
+    public function new(_id:String, _name:String)
+    {
+        this.name = _name;
+        this.id   = _id;
+    }
+}
