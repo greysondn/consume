b7916d0afcfdd33d49de9dd3f827fdf2ce11d7ac
J. 'Dorian Greyson' L <greysondn@gmail.com>
2019-07-02 09:29:55 -0400

    BodyPartCollection - It's a thing, yeah

---
+++ source/net/darkglass/iguttae/gameworld/character/body/BodyPartCollection.hx
@@ -0,0 +1,63 @@
+package net.darkglass.iguttae.gameworld.character.body;
+
+/**
+ * Sensible storage for body parts
+ */
+class BodyPartCollection
+{
+    /**
+     * The contents of this container.
+     */
+    private var contents:Array<BodyPart> = [];
+
+    public function new()
+    {
+        // that's it.
+    }
+
+    public function add(part:BodyPart):Bool
+    {
+        // eventual return
+        var ret:Bool = true;
+
+        // put item in inventory
+        this.contents.push(part);
+
+        // return
+        return ret;
+    }
+
+    public function remove(part:BodyPart):Bool
+    {
+        // one liner
+        return this.contents.remove(part);
+    }
+
+    public function list():Array<BodyPart>
+    {
+        return this.contents;
+    }
+
+    public function count(_id:String):Int
+    {
+        // eventual return
+        var ret:Int = 0;
+
+        // iterate
+        for (part in this.contents)
+        {
+            if (part.id == _id)
+            {
+                ret = ret + 1;
+            }
+        }
+
+        // done
+        return ret;
+    }
+
+    public function has(_id:String):Bool
+    {
+        return (this.count(_id) > 0);
+    }
+}
