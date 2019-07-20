e7c9be73d76cc4d1659863a19c67289700ec9751
J. 'Dorian Greyson' L <greysondn@gmail.com>
2019-07-19 23:29:21 -0400

    Species - seems to load, not really making use of yet

--- source/net/darkglass/iguttae/loader/YamlLoader.hx
+++ source/net/darkglass/iguttae/loader/YamlLoader.hx
@@ -3,6 +3,7 @@ package net.darkglass.iguttae.loader;
 import net.darkglass.iguttae.gameworld.actor.Actor;
 import net.darkglass.iguttae.gameworld.character.Species;
 import net.darkglass.iguttae.gameworld.character.body.BodyPart;
+import net.darkglass.iguttae.gameworld.character.body.Measurement;
 import net.darkglass.iguttae.gameworld.item.Item;
 import net.darkglass.iguttae.environment.Environment;
 import net.darkglass.iguttae.gameworld.map.Room;
@@ -65,7 +66,7 @@ class YamlLoader
                             ?tranSrc:String="assets/data/en-us/doors.yaml",
                             ?itemSrc:String="assets/data/en-us/items.yml",
                             ?spwnSrc:String="assets/data/en-us/spawns.yaml",
-                            ?speciesSrc:String="assets/data/en-us/species.yaml"
+                            ?speciesSrc:String="assets/data/en-us/species.yml"
                         ):Void
     {
         // this typing is bad and there's no elegant fix
@@ -262,7 +263,76 @@ class YamlLoader
                 {
                     swp.bodyParts.add(new BodyPart("sphincter", entry.get("parts").get("desc").get("sphincter")));
                 }
+
+                // measures
+                swp.height.min = entry.get("measures").get("height").get("min");
+                swp.height.max = entry.get("measures").get("height").get("max");
+
+                swp.weight.min = entry.get("measures").get("weight").get("min");
+                swp.weight.max = entry.get("measures").get("weight").get("max");
+
+                swp.chest.min = entry.get("measures").get("chest").get("min");
+                swp.chest.max = entry.get("measures").get("chest").get("max");
+
+                swp.waist.min = entry.get("measures").get("waist").get("min");
+                swp.waist.max = entry.get("measures").get("waist").get("max");
+
+
+                swp.hips.min = entry.get("measures").get("hips").get("min");
+                swp.hips.max = entry.get("measures").get("hips").get("max");
+
+                swp.butt.min = entry.get("measures").get("butt").get("min");
+                swp.butt.max = entry.get("measures").get("butt").get("max");
+
+                // start measures
+                swp.breasts.measure = entry.get("start").get("measures").get("breasts");
+                swp.testes.measure  = entry.get("start").get("measures").get("balls");
+
+                // pingus starts.
+                var pingus:BodyPart = new BodyPart("penis", "penis");
+                pingus.isPhallic    = true;
+                pingus.pMultiplier  = entry.get("start").get("measures").get("erect");
+                pingus.pLength      = entry.get("start").get("measures").get("penis").get("length");
+                pingus.pWidth       = entry.get("start").get("measures").get("penis").get("width");
+                swp.bodyParts.add(pingus);
+
+                // capacities
+                swp.stomach.capacity = entry.get("start").get("capacity").get("stomach");
+                swp.bowels.capacity  = entry.get("start").get("capacity").get("bowels");
+                swp.breasts.capacity = entry.get("start").get("capacity").get("breasts");
+                swp.testes.capacity  = entry.get("start").get("capacity").get("testes");
+
+                // gains
+                swp.stomach.gains    = entry.get("start").get("gains").get("fat");
+                swp.breasts.gains    = entry.get("start").get("gains").get("milk");
+                swp.testes.gains     = entry.get("start").get("gains").get("spunk");
+
+                // digestion damage
+                swp.stomach.damage   = entry.get("start").get("digestion").get("damage");
+                swp.breasts.damage   = entry.get("start").get("digestion").get("damage");
+                swp.testes.damage    = entry.get("start").get("digestion").get("damage");
+
+                // stretch time
+                swp.stomach.stretchTime = entry.get("start").get("stretch").get("time").get("stomach");
+                swp.bowels.stretchTime  = entry.get("start").get("stretch").get("time").get("bowels");
+                swp.breasts.stretchTime = entry.get("start").get("stretch").get("time").get("breasts");
+                swp.testes.stretchTime  = entry.get("start").get("stretch").get("time").get("testes");
+
+                // stretch amount
+                swp.stomach.stretchAmount = entry.get("start").get("stretch").get("amount").get("stomach");
+                swp.bowels.stretchAmount  = entry.get("start").get("stretch").get("amount").get("bowels");
+                swp.breasts.stretchAmount = entry.get("start").get("stretch").get("amount").get("breasts");
+                swp.testes.stretchAmount  = entry.get("start").get("stretch").get("amount").get("testes");
+
+                // das it, add it!
+                env.species.add(swp);
             }
+        }
+
+        // should all be loaded, integrity check
+        if (!env.species.checkIntegrity())
+        {
+            throw "HEY DUMMY YOU BOTCHED THE SPECIES LIST, FIX IT";
         }
     }
 
